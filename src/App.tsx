585d5accd60654c413ab0678009959da9ec1148a
adamabiyuu <advmabiyu@gmail.com>
2026-08-04 16:02:44 +0700

    setup project aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import Navbar from "@/components/layout/Navbar";
+
 function App() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-slate-950">
-      <h1 className="text-5xl font-bold text-cyan-400">Hello Portfolio 🚀</h1>
-    </main>
+    <>
+      <Navbar />
+
+      <main className="flex min-h-screen items-center justify-center bg-slate-950">
+        <h1 className="text-5xl font-bold text-cyan-400">Hello Portfolio 🚀</h1>
+      </main>
+    </>
   );
 }
 
