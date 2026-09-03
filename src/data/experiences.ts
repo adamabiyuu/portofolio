874b4f29f4425e404eeeab87c58666d5c4942956
adamabiyuu <advmabiyu@gmail.com>
2026-09-03 18:25:36 +0700

    fix data/experiences.ts

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -4,27 +4,19 @@ export const experiences: Experience[] = [
   {
     id: 2,
     year: "2026",
-    duration: "Agustus 2026 – September 2026",
+    duration: "August 2026 – September 2026",
     company: "Saiket Systems",
-    position: "Frontend Web Developer Intern",
+    position: "Frontend Developer Intern",
     description:
       "Developed responsive and interactive web applications using React, TypeScript, Tailwind CSS, HTML, CSS, and JavaScript through hands-on projects, including blog layouts, product cards, quiz applications, To-Do List applications, and a modern SaaS landing page. Enhanced the final project using React with reusable components, responsive design, interactive UI elements, form validation, animations, and modern front-end development practices, while focusing on usability, maintainability, and cross-device compatibility.",
   },
   {
     id: 1,
-    year: "2025",
-    duration: "Agustus 2025 – September 2025",
+    year: "2024",
+    duration: "July 2024 – August 2024",
     company: "Dinas Pendidikan Kota Cilegon",
     position: "Frontend Web Developer Intern",
     description:
       "Developed a web-based application for managing student data, including features for data entry, retrieval, and reporting. Collaborated with a team of developers to implement responsive design and ensure cross-browser compatibility.",
   },
-  // {
-  //   id: 2,
-  //   year: "2023",
-  //   duration: "Jul 2023 – Nov 2024",
-  //   company: "PT Selago Makmur Plantation",
-  //   position: "Commodity Staff",
-  //   description: "Handled commodity operations, monitored production processes, prepared operational reports, and collaborated with cross-functional teams to maintain productivity.",
-  // },
 ];
